1d056d1575b4aecb609c530e4d01b7877ef04bc8
Grzegorz Prawdzik <g.prawdzik@gmail.com>
2017-12-26 17:04:40 +0100

    BigData

---
+++ genthrift.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+rm -rf gen-javabean src/java/manning/schema
+thrift -r --gen java:beans,hashcode,nocamel src/schema.thrift
+mv gen-javabean/manning/schema src/java/manning/
+rm -rf gen-javabean
